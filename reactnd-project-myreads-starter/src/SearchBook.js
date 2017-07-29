5eb4faab57e428b50baddc11d68435201adc5a01
jlcoto <jscoto@gmail.com>
2017-07-29 15:54:37 +0200

    Selection of book to my reads erases selection on search page.

--- reactnd-project-myreads-starter/src/SearchBook.js
+++ reactnd-project-myreads-starter/src/SearchBook.js
@@ -19,16 +19,12 @@ class SearchBook extends Component {
 		let booksInShelf = this.booksInShelf()
 		let newQuery = query.trim();
 		this.setState({query: newQuery})
-
-
 		if (newQuery !== ''){
 			BooksAPI.search(newQuery, 5).then((books) => {
-				console.log(booksInShelf)
 				if (!books.error) {
 						let newBooks = books.filter(function(book) {
-							console.log(booksInShelf.indexOf(book.id) !==-1)
-							return booksInShelf.indexOf(book.id) ===-1
-						})
+								return booksInShelf.indexOf(book.id) ===-1
+							})
 						newBooks.map((book) => {
 							 return book.shelf = 'none';
 							})
@@ -40,16 +36,13 @@ class SearchBook extends Component {
 		}
 	}
 
-
-
-
-		// this.setState((state) => ({booksFound: state.booksFound.filter((book) => !booksInShelf.includes(book))}))
-
-
+	selectBook = (book, shelf) => {
+		this.setState((state) => ({booksFound: state.booksFound.filter((b) => b.id !== book.id)}))
+		this.props.moveToMyRead(book, shelf)
+	}
 
 	render() {
 
-		const { moveToMyRead, booksInMyReads} = this.props
 		const { query, booksFound } = this.state
 
 
@@ -60,14 +53,6 @@ class SearchBook extends Component {
         	    <div className="search-books-bar">
         	      <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
         	      <div className="search-books-input-wrapper">
-        	        {/*
-        	        //   NOTES: The search from BooksAPI is limited to a particular set of search terms.
-        	        //   You can find these search terms here:x
-        	        //   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-        	        //   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-        	        //   you don't find a specific author or title. Every search is limited by search terms.
-        	        // */}
         	        <input
         	        	type="text"
 						placeholder="Search by title or author"
@@ -77,7 +62,7 @@ class SearchBook extends Component {
         	      </div>
         	    </div>
         	    <div className="search-books-results">
-        	    	<ListReads books={booksFound} onUpdateShelf={(book, shelf) => moveToMyRead(book, shelf)}/>
+        	    	<ListReads books={booksFound} onUpdateShelf={this.selectBook.bind(this)}/>
         	    </div>
         	</div>)
 	}
